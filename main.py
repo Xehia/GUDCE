19996d59fa660985abbe25a050eaa3027dfa7634
Xehia <matteoperini01@gmail.com>
2024-01-28 20:25:42 +0100

    Token fix

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 import logging
+from dotenv import load_dotenv
+import os
 from commands import *
 from telegram.ext import ApplicationBuilder, CallbackQueryHandler
+
+load_dotenv()
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     level=logging.WARNING)
 
 
 if __name__ == '__main__':
-    application = ApplicationBuilder().token('').build()
+    application = ApplicationBuilder().token(os.environ.get('TOKEN')).build()
 
     # Add command handlers
     application.add_handler(help_handler)
